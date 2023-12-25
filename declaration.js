f82a844757ef7fbfe1ec361ba1eab2a66046723f
berivan <berivantaskin4018@gmail.com>
2023-12-25 23:27:49 +0300

    functions

--- declaration.js
+++ declaration.js
@@ -42,9 +42,48 @@ yazdir("canan")
 yazdir()
 
 
+// VERİ DÖNDÜREN (RETURN) FONK.
+
+function bol (a,b){
+      const res = a / b
+      return res
+}
+
+let sonuc = bol(10,2)
+console.log("sonuc=" , sonuc);
+
+
+//ÖRNEK
+
+function toplama (n1,n2,n3){
+      return n1 + n2 +n3
+}
+
+console.log(`SONUC:${toplama(3,5,7)}`);
+
+//2.ÖRNEK
+//? Ornek:Tek-Cift
+//? Console'dan girilen bir sayinin tek veya cift oldgunu
+//? hesaplayan ve sonucu ana programa donduren kodu fonksiyonlar ile yaziniz.
 
 
 
 
+function tekVeyaCift (num) {
+    return num % 2 === 0 ? "cıft" : "tek"
+}
+
+const sayi = Number(prompt("bir sayı giriniz")) 
+console.log(`${sayi} ${tekVeyaCift(sayi)} dir`);
+
+//1. yöntemdekı fonk.konumu
+
+yaz()
 
 
+function yaz(){
+    console.log("merhaba");
+}
+
+yaz()
+yaz()
